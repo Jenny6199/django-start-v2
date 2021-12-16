bcaccb4042ea171b7d82ef1f1f669f0768372389
Jenny6199 <Jenny6199@yandex.ru>
2021-12-17 02:38:04 +0300

    inc-categories-menu.html create

--- src/geekshop/mainapp/views.py
+++ src/geekshop/mainapp/views.py
@@ -16,16 +16,24 @@ def products(request):
     ]
 
     small_images = [
-    {'href': '#', 'image': 'img/controll.jpg'},
-    {'href': '#', 'image': 'img/controll1.jpg'},
-    {'href': '#', 'image': 'img/controll2.jpg'},
+        {'href': '#', 'image': 'img/controll.jpg'},
+        {'href': '#', 'image': 'img/controll1.jpg'},
+        {'href': '#', 'image': 'img/controll2.jpg'},
+    ]
 
+    links_menu = [
+        {'href': 'products', 'name': 'все'},
+        {'href': 'products', 'name': 'дом'},
+        {'href': 'products', 'name': 'офис'},
+        {'href': 'products', 'name': 'модерн'},
+        {'href': 'products', 'name': 'классика'},
     ]
 
     content = {
         'title': title,
         'related_products': related_products,
         'small_images': small_images,
+        'links_menu': links_menu
     }
 
     return render(request, 'mainapp/products.html', content)
