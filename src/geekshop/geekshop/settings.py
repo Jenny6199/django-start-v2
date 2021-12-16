677f4a39129a2c498c4c1c487aeb9c2ac5d48f70
Jenny6199 <Jenny6199@yandex.ru>
2021-12-16 23:26:16 +0300

    staticfiles_dirs in settings.py and modified templates

--- src/geekshop/geekshop/settings.py
+++ src/geekshop/geekshop/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -122,8 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIR = (
-    os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
 )
 
 # Default primary key field type
