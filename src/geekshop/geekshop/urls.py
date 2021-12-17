72506714ba6e2bfb6cf03c88c651a48c8f8c9949
Jenny6199 <Jenny6199@yandex.ru>
2021-12-17 03:35:46 +0300

    add settings.DEBUG in urls.py

--- src/geekshop/geekshop/urls.py
+++ src/geekshop/geekshop/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import mainapp.views as mainapp
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
@@ -23,3 +25,6 @@ urlpatterns = [
     path('products/', mainapp.products, name='products'),
     path('contact/', mainapp.contact, name='contact'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
