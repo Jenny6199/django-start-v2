29c63fd4643512b0832b8f5c74e1650426b8fa75
Jenny6199 <Jenny6199@yandex.ru>
2021-12-22 19:17:19 +0300

    add adminapp.urls in geekshop/urls.py

--- src/geekshop/geekshop/urls.py
+++ src/geekshop/geekshop/urls.py
@@ -17,12 +17,13 @@ from django.contrib import admin
 from django.urls import path
 import mainapp.views as mainapp
 import basketapp.views as basketapp
+import adminapp.views as adminapp
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls, name='admin'),
+    path('admin/', include('adminapp.urls'), name='admin'),
     path('', mainapp.main, name='main'),
     path('products/', include('mainapp.urls', namespace='products')),
     path('contact/', mainapp.contact, name='contact'),
