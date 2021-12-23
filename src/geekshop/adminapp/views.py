7ee772ea78d6d253c0733efe0fcf9c7a0fad32b3
Jenny6199 <Jenny6199@yandex.ru>
2021-12-23 14:28:57 +0300

    creage empty def user_create in adminapp/views.py

--- src/geekshop/adminapp/views.py
+++ src/geekshop/adminapp/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from authapp.models import ShopUser
+from mainapp.models import Product, ProductCategory
 
-# Create your views here.
+
+def users(request):
+    title = 'админка/пользователи'
+
+    users_list = ShopUser.objects.all().order_by('-is_active', '-is_superuser', 'is_staff', username)
+
+    content = {
+        'title': title,
+        'objects': users_list,
+    }
+
+    return render(request, 'adminapp/users.html', content)
+
+
+def user_create(request):
+    pass
+
