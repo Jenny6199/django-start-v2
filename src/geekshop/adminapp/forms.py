34478b8c3064ce18190b1fbf35c35576cfc07a4e
Jenny6199 <Jenny6199@yandex.ru>
2021-12-24 18:58:13 +0300

    makemigrations and migrate

--- src/geekshop/adminapp/forms.py
+++ src/geekshop/adminapp/forms.py
@@ -14,7 +14,7 @@ class ProductCategoryEditForm(forms.ModelForm):
         fields = '__all__'
 
         def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs):
+            super().__init__(*args, **kwargs)
             for field_name, field in self.fields.items():
                 field.widget.attrs['class'] = 'form-controle'
                 field.help_text = ''
