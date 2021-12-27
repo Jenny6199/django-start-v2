5273e279387b000ce5979a5d2bff0ebb6fc560a2
Jenny6199 <Jenny6199@yandex.ru>
2021-12-28 01:08:08 +0300

    add ProductEditForm in mainapp/forms.py

--- src/geekshop/mainapp/forms.py
+++ src/geekshop/mainapp/forms.py
@@ -1,8 +1,9 @@
 from django.forms import forms
 from .models import ProductCategory
+from mainapp.models import Product
 
 
-class ProductCategoryCreateForm(forms.Form):
+class ProductCategoryEditForm(forms.Form):
     class Meta:
         model = ProductCategory
         fields = '__all__'
@@ -11,3 +12,15 @@ class ProductCategoryCreateForm(forms.Form):
         super().__init__(*args, **kwargs)
         for field_name, field in self.field.items():
             field.wodget.attrs['class'] = 'form-control'
+
+
+class ProductEditForm(forms.Form):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
