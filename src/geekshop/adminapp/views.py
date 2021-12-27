501a7e27c42a909189a77796b79e442fa7fb3812
Jenny6199 <Jenny6199@yandex.ru>
2021-12-28 02:00:23 +0300

    add def product_update in adminapp/views.py

--- src/geekshop/adminapp/views.py
+++ src/geekshop/adminapp/views.py
@@ -184,24 +184,37 @@ def product_create(request, pk):
         if product_form.is_valid():
             product_form.save()
             return HttpResponseRedirect(reverse('admin:products', args=[pk]))
-        else:
-            product_form = ProductEditForm(initial={'category': category})
-            content = {
-                'title': title,
-                'update_form': product_form,
-                'category': category,
-            }
+    else:
+        product_form = ProductEditForm(initial={'category': category})
 
-            return render(request, 'adminapp/product_update.html', content)
+    content = {
+        'title': title,
+        'update_form': product_form,
+        'category': category,
+    }
 
-
-
-def product_read(request, pk):
-    pass
+    return render(request, 'adminapp/product_update.html', content)
 
 
 def product_update(request, pk):
-    pass
+    title = 'продукт/редактирование'
+    edit_product = get_object_or_404(Product, pk=pk)
+
+    if request.method == 'POST':
+        edit_form = ProductEditForm(request.POST, request.FILES, instance=edit_product)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('admin:product_update', args=[edit_product.pk]))
+    else:
+        edit_form = ProductEditForm(instance=edit_product)
+
+    content = {
+        'title': title,
+        'update_form': edit_form,
+        'category': edit_product.category
+    }
+
+    return render(request, 'adminapp/product_update.html', content)
 
 
 def product_delete(request, pk):
