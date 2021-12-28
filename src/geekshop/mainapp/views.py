96ca9e6b2d74128519b4eb9c28117cfb37fd0783
Jenny6199 <Jenny6199@yandex.ru>
2021-12-28 14:11:36 +0300

    add paginator in views.py didnt work __is_active

--- src/geekshop/mainapp/views.py
+++ src/geekshop/mainapp/views.py
@@ -35,9 +35,7 @@ def main(request):
     return render(request, 'mainapp/index.html', content)
 
 
-def products(request, pk=None):
-    print(pk)
-
+def products(request, pk=None, page=1):
     title = 'продукты'
     links_menu = ProductCategory.objects.filter(is_active=True)
     basket = get_basket(request.user)
@@ -48,17 +46,35 @@ def products(request, pk=None):
 
     if pk is not None:
         if pk == 0:
-            category = {'name': 'все'}
-            our_products = Product.objects.all().order_by('price')
+            category = {
+                'pk': 0,
+                'name': 'все',
+            }
+            our_products = Product.objects.filter(
+                is_active=True,
+                category__is_active=True,
+            ).order_by('price')
         else:
             category = get_object_or_404(ProductCategory, pk=pk)
-            our_products = Product.objects.filter(category__pk=pk).order_by('price')
+            our_products = Product.objects.filter(
+                category__pk=pk,
+                is_active=True,
+                category__is_active=True,
+            ).order_by('price')
+
+        paginator = Paginator(our_products, 2)
+        try:
+            products_paginator = paginator.page(page)
+        except PageNotAnInteger:
+            products_paginator = paginator.page(1)
+        except EmptyPage:
+            products_paginator = paginator.page(paginator.num_pages)
 
         content = {
             'title': title,
             'links_menu': links_menu,
             'category': category,
-            'products': our_products,
+            'products': products_paginator,
             'basket': basket,
         }
 
