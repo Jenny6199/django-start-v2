66123791b7af9ca3eed48ca442804bc799800fb6
Jenny6199 <Jenny6199@yandex.ru>
2021-12-28 19:10:26 +0300

    add DeleteView in adminapp/views.py

--- src/geekshop/adminapp/views.py
+++ src/geekshop/adminapp/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse, reverse_lazy
 from authapp.forms import ShopUserRegisterForm
 from adminapp.forms import ShopUserAdminEditForm, ProductCategoryEditForm, ProductEditForm
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.utils.decorators import method_decorator
 
 
@@ -37,6 +37,20 @@ class ProductCategoryUpdateView(UpdateView):
         context['title'] = 'категории/редактирование'
 
         return context
+
+
+class ProductCategoryDeleteView(DeleteView):
+    model = ProductCategory
+    template_name = 'adminapp/category_delete.html'
+    success_url = reverse_lazy('admin:categories')
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.is_active = False
+        self.object.save()
+
+        return HttpResponseRedirect(self.get_success_url())
+
 
 @user_passes_test(lambda u: u.is_superuser)
 def users(request):
