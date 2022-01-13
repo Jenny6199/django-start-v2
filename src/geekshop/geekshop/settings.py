2005835d903f6a444949dd580e209bcca1bd6d62
Jenny6199 <Jenny6199@yandex.ru>
2022-01-13 23:18:13 +0300

    server dont work, change settings.py again

--- src/geekshop/geekshop/settings.py
+++ src/geekshop/geekshop/settings.py
@@ -145,15 +145,15 @@ LOGIN_URL = '/auth/login/'
 DOMAIN_NAME = 'http://localhost:8000'
 
 # 1. Вариант отправки сообщений на реальный сервер.
-# EMAIL_HOST = 'localhost'
-# EMAIL_PORT = '25'
-# EMAIL_HOST_USER = 'django@geekshop.local'
-# EMAIL_HOST_PASSWORD = 'geekshop'
-# EMAIL_USE_SSL = False
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = '25'
+EMAIL_HOST_USER = 'django@geekshop.local'
+EMAIL_HOST_PASSWORD = 'geekshop'
+EMAIL_USE_SSL = False
 
 #   2. Вариант python -m smtpd -n -c DebuggingServer localhost:25
 #   EMAIL_HOST_USER, EMAIL_HOST_PASSWORD = None, None
 
-  # Вариант логирования сообщений почты в виде файлов вместо отправки
-  EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-  EMAIL_FILE_PATH = 'tmp/email-messages/
+#   Вариант логирования сообщений почты в виде файлов вместо отправки
+#   EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+#   EMAIL_FILE_PATH = 'tmp/email-messages/
