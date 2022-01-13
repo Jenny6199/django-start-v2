3d6ba08af0cd3efb761efd3773b7db67cadfb3ef
Jenny6199 <Jenny6199@yandex.ru>
2022-01-13 23:30:55 +0300

    try again

--- src/geekshop/geekshop/settings.py
+++ src/geekshop/geekshop/settings.py
@@ -147,13 +147,13 @@ DOMAIN_NAME = 'http://localhost:8000'
 # 1. Вариант отправки сообщений на реальный сервер.
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = '25'
-EMAIL_HOST_USER = 'django@geekshop.local'
-EMAIL_HOST_PASSWORD = 'geekshop'
+# EMAIL_HOST_USER = 'django@geekshop.local'
+# EMAIL_HOST_PASSWORD = 'geekshop'
 EMAIL_USE_SSL = False
 
 #   2. Вариант python -m smtpd -n -c DebuggingServer localhost:25
-#   EMAIL_HOST_USER, EMAIL_HOST_PASSWORD = None, None
+EMAIL_HOST_USER, EMAIL_HOST_PASSWORD = None, None
 
 #   Вариант логирования сообщений почты в виде файлов вместо отправки
-#   EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-#   EMAIL_FILE_PATH = 'tmp/email-messages/
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = 'tmp/email-messages/'
