62a6e91e6973ad5a9623b5637a8f664e1fb379a3
Jenny6199 <Jenny6199@yandex.ru>
2022-01-20 00:15:02 +0300

    make migrations, add path and button for VK in authapp/login.html

--- src/geekshop/geekshop/urls.py
+++ src/geekshop/geekshop/urls.py
@@ -23,19 +23,14 @@ from django.conf.urls.static import static
 from django.conf.urls import include
 
 urlpatterns = [
-    path('admin/', include('adminapp.urls', namespace='admin')),
+    re_path(r'^admin/', include('adminapp.urls', namespace='admin')),
     re_path(r'^$', mainapp.main, name='main'),
     re_path(r'^products/', include('mainapp.urls', namespace='products')),
     re_path(r'^contact/', mainapp.contact, name='contact'),
     re_path(r'^empty/', mainapp.products, name='empty'),
     re_path(r'^auth/', include('authapp.urls',  namespace='auth')),
     re_path(r'^basket/', include('basketapp.urls', namespace='basket')),
-    # path('', mainapp.main, name='main'),
-    # path('products/', include('mainapp.urls', namespace='products')),
-    # path('contact/', mainapp.contact, name='contact'),
-    # path('empty/', mainapp.products, name='empty'),
-    # path('auth/', include('authapp.urls', namespace='auth')),
-    # path('basket/', include('basketapp.urls', namespace='basket')),
+    path('', include('social_django.urls', namespace='social')),
 ]
 
 if settings.DEBUG:
