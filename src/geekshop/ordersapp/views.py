d443257e5c0ecf237f618025d88d5417b4009485
Jenny6199 <Jenny6199@yandex.ru>
2022-01-25 19:48:58 +0300

    dont complite troubleshooting

--- src/geekshop/ordersapp/views.py
+++ src/geekshop/ordersapp/views.py
@@ -1,13 +1,11 @@
-
 from django.shortcuts import get_object_or_404, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.db import transaction
 from django.forms import inlineformset_factory
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-
 from basketapp.models import Basket
 from ordersapp.models import Order, OrderItem
-from ordersapp.forms import OrderItemForm
+from ordersapp.forms import OrderItemForm, OrderForm
 
 
 class OrderList(ListView):
@@ -87,25 +85,18 @@ class OrderItemsUpdate(UpdateView):
     success_url = reverse_lazy('ordersapp:orders_list')
 
     def get_context_data(self, **kwargs):
-        data = super(OrderItemsCreate, self).get_context_data(**kwargs)
+        data = super(OrderItemsCreate, self).get_context_data(instance=self.object)
+
         OrderFormSet = inlineformset_factory(
             Order,
             OrderItem,
             form=OrderItemForm,
             extra=1)
         if self.request.POST:
-            formset = OrderFormSet(self.request.POST)
+            data['orderitems'] = OrderFormSet(self.request.POST, instance=self.object)
         else:
-            basket_items = Basket.get_items(self.request.user)
-            if len(basket_items):
-                OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=len(basket_items))
-                formset = OrderFormSet()
-                for num, form in enumerate(formset.forms):
-                    form.initial['product'] = basket_items[num].product
-                    form.initial['quantity'] = basket_items[num].quantity
-                    basket_items.delete()
-            else:
-                formset = OrderFormSet()
+            data['orderitems'] = OrderFormSet(instance=self.object)
+
         data['orderitems'] = formset
         return data
 
@@ -114,7 +105,6 @@ class OrderItemsUpdate(UpdateView):
         orderitems = context['orderitems']
 
         with transaction.atomic():
-            form.instance.user = self.request.user
             self.object = form.save()
             if orderitems.is_valid():
                 orderitems.instance = self.object
