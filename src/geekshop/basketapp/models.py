4a5a3097465a73d416ad3402b0b5ef46a3bd9048
Jenny6199 <Jenny6199@yandex.ru>
2022-01-30 14:23:32 +0300

    change quantity on warehouse and add button in product_update.html

--- src/geekshop/basketapp/models.py
+++ src/geekshop/basketapp/models.py
@@ -3,11 +3,22 @@ from django.conf import settings
 from mainapp.models import Product
 
 
+class BasketQuerySet(models.QuerySet):
+
+    def delete(self, *args, **kwargs):
+        for object in self:
+            object.product.quantity += object.quantity
+            object.product.save()
+        super(BasketQuerySet, self).delete(*args, **kwargs)
+
+
 class Basket(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='basket')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
     add_datetime = models.DateTimeField(verbose_name='время', auto_now_add=True)
+
+    objects = BasketQuerySet.as_manager()
 
     def __str__(self):
         return f'{self.product.name}, ({self.quantity})'
