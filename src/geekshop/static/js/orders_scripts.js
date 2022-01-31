e61cd1490bf6e35bbfe5dc822046620b018a9aee
Jenny6199 <Jenny6199@yandex.ru>
2022-02-01 01:04:11 +0300

    add function deleteOrderItem in orders_scripts.js

--- src/geekshop/static/js/orders_scripts.js
+++ src/geekshop/static/js/orders_scripts.js
@@ -55,15 +55,29 @@ window.onload = function () {
         }
     });
 
-    $('.order_form').on('click', 'input[type="checkbox"]', function(){
-        var target = event.target;
-        orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-DELETE', ''));
-        if (target.checked) {
-            delta_quantity = -quantity_arr[orderitem_num];
-        } else {
+//    $('.order_form').on('click', 'input[type="checkbox"]', function(){
+//        var target = event.target;
+//        orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-DELETE', ''));
+//        if (target.checked) {
+//            delta_quantity = -quantity_arr[orderitem_num];
+//        } else {
+//        delta_quantity = -quantity_arr[orderitem_num];
+//        }
+//        orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
+//        console.log('input-checkbox func finish');
+//    });
+
+    function deleteOrderItem(row) {
+        var target_name = row[0].querySelector('input[type="number"]').name;
+        orderitem_num = parseInt(target_name.replace('orderitems-', '').replace('-quantity', ''));
         delta_quantity = -quantity_arr[orderitem_num];
-        }
         orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
-        console.log('input-checkbox func finish');
+    }
+
+    $('.formset_row').formset({
+        addText: 'добавить продукт',
+        deleteText: 'удалить',
+        prefix: 'orderitems',
+        removed: deleteOrderItem
     });
 }
