4b173ba2d4703df05b52ca9cce82206146196608
Jenny6199 <Jenny6199@yandex.ru>
2022-02-01 02:07:34 +0300

    dont finish task_4 pause

--- src/geekshop/static/js/orders_scripts.js
+++ src/geekshop/static/js/orders_scripts.js
@@ -55,18 +55,6 @@ window.onload = function () {
         }
     });
 
-//    $('.order_form').on('click', 'input[type="checkbox"]', function(){
-//        var target = event.target;
-//        orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-DELETE', ''));
-//        if (target.checked) {
-//            delta_quantity = -quantity_arr[orderitem_num];
-//        } else {
-//        delta_quantity = -quantity_arr[orderitem_num];
-//        }
-//        orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
-//        console.log('input-checkbox func finish');
-//    });
-
     function deleteOrderItem(row) {
         var target_name = row[0].querySelector('input[type="number"]').name;
         orderitem_num = parseInt(target_name.replace('orderitems-', '').replace('-quantity', ''));
@@ -81,8 +69,10 @@ window.onload = function () {
         removed: deleteOrderItem
     });
 
-    $('.order_form select').change(function(){
-        var target = event.target;
-        console.log(target);
-    });
+//    $('.order_form select').change(function(){
+//        let target = event.target;
+//        console.log(target);
+//    });
+
+//    select_number = parseInt(target.name.replace('orderitems-', '').replace('-product', ''));
 }
