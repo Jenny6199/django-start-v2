49770e7153f3e69df0f49deb854843f937415576
Jenny6199 <Jenny6199@yandex.ru>
2022-02-01 12:40:45 +0300

    fill orders_scripts.js add func orderSummaryRecalc and add path in url.py

--- src/geekshop/static/js/orders_scripts.js
+++ src/geekshop/static/js/orders_scripts.js
@@ -69,10 +69,44 @@ window.onload = function () {
         removed: deleteOrderItem
     });
 
-//    $('.order_form select').change(function(){
-//        let target = event.target;
-//        console.log(target);
-//    });
+    function orderSummaryRecalc () {
+        order_total_quantity = 0;
+        order_total_cost = 0;
 
-//    select_number = parseInt(target.name.replace('orderitems-', '').replace('-product', ''));
+        for (var i=0; i < TOTAL_FORMS; i++) {
+            order_total_quantity += quantity_arr[i];
+            order_total_cost += quantity_arr[i] * price_arr[i];
+        }
+        $('.order_total_quantity').html(order_total_quantity.toString());
+        $('.order_total_cost').html(Number(order_total_cost.toFixed(2)).toStirng());
+    }
+
+    $('.order_form select').change(function(){
+        let target = event.target;
+        orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-product', ''));
+        var orderitem_product_pk = target.options[target.selectedIndex].value;
+
+        if (orderitem_product_pk) {
+            $.ajax({
+                url:"/order/product/" + orderitem_product_pk + "/price/",
+                success: function (data) {
+                    if (data.price) {
+                        price[orderitem_num] = parseFloat(data.price);
+                        if (isNan(quantity_arr[orderitem_num])) {
+                            quantity_arr[orderitem_num] = 0;
+                        }
+                        var price_html = '<span>' + data.price.toString().replace('.', ',') + '</span> руб.';
+                        var current_tr = $('.order_form table').find('tr:eq(' + (orderitem_num + 1) +')');
+
+                        current_tr.find('td:eq(2)').html(price_html);
+
+                        if (isNan(current_tr.find('input[type="number"]').val())) {
+                            current_tr.find('input[type="number"]').val(0);
+                        }
+                        orderSummaryRecalc();
+                    }
+                },
+            });
+        }
+    });
 }
