8a6bb1d03e400a2c1bed84648760d6ccad977732
Jenny6199 <Jenny6199@yandex.ru>
2022-02-01 14:45:15 +0300

    change database in settings.py

--- src/geekshop/geekshop/settings.py
+++ src/geekshop/geekshop/settings.py
@@ -95,8 +95,14 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'geekshop',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'django',
+        'PASSWORD': 'geekbrains',
+        'HOST': 'localhost'
+        # previews:
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
