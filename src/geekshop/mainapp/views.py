9359b35e8ca33546ca706a1959d8460d7acf4e41
Jenny6199 <Jenny6199@yandex.ru>
2022-02-11 01:49:38 +0300

    add def get_products in mainapp/views.py

--- src/geekshop/mainapp/views.py
+++ src/geekshop/mainapp/views.py
@@ -132,3 +132,16 @@ def get_category(pk):
         return category
     else:
         return get_object_or_404(ProductCategory, pk=pk)
+
+
+def get_products():
+    if settings.LOW_CACHE:
+        key = 'products'
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(is_active=True, category__is_active=True).select_related('category')
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.filter(is_active=True, category__is_active=True).select_related('category')
+
